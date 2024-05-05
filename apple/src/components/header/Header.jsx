f544ef0f8f8fe71bde393fc77f461f266267d171
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-05 22:09:10 +0545

    header added

---
+++ apple/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+
+function Header(){
+    return(
+        <>
+        <div className="w-full h-24 bg-[#171717]">'
+        <div className="list-image-[url(WhatsApp Image 2024-04-22 at 12.37.57_bf80c8b2.jpg)]">
+            
+        </div>
+        </div>
+        </>
+    )
+}
+export default Header
