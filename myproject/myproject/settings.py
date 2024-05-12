e7532b176e314e220997d49837602ea8b113d33a
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-12 17:03:58 +0545

    all changes working fine

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -52,10 +52,16 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'apple',
+        'USER': 'root',
+        'PASSWORD': 'adminBibek',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
+
 
 LANGUAGE_CODE = 'en-us'
 
