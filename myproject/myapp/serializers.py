840c0fa53f1f9f2f2b9b27e5788bd2c26c1448e9
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-15 12:33:00 +0545

    Serailizers created to generate api

--- myproject/myapp/serializers.py
+++ myproject/myapp/serializers.py
@@ -1,1 +1,12 @@
 from  rest_framework import serializers 
+from  myapp.models import Category , Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['category_name', 'category_id']
+        
+class ProductSerializer(serializers.ModelSerializer):  ###blueprint of an object
+    class Meta:
+        model = Product
+        fields= [ 'category','name','picture','description','price_in_npr']
