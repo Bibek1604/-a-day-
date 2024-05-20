e9a91c2972ad2e5f82c8f91a8afdb8d474b6ec7b
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-20 23:31:47 +0545

    api executed

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+
+class ProductListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path  # Import include
+from myapp.views import (ProductListCreateAPIView,
+                         ProductRetrieveUpdateDestroyAPIView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('myapp/', include('myapp.urls')),  # Include URLs for your app
+    path('', ProductListCreateAPIView.as_view(), name='product-list-create'),
+    path('products/<int:pk>/', ProductRetrieveUpdateDestroyAPIView.as_view(), name='product-detail'),
 ]
+
+    # path('myapp/', include('myapp.urls')),  # Include URLs for your app
