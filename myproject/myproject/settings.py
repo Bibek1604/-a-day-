0dc6801b990bfb4f2731cb38083013d692cee988
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-20 23:39:46 +0545

    sql database added

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'appleaday',
+        'USER': 'root',
+        'PASSWORD': 'adminBibek',
+        'HOST': '127.0.0.1',   # Or the IP address where MySQL is hosted
+        'PORT': '3306',        # Default MySQL port
     }
 }
+
 
 
 # Password validation
