114fa17182a8a04f5191ce8199701cf3cf390c00
Bibek Pandey <145880788+imBibek-Pandey@users.noreply.github.com>
2024-05-21 14:42:22 +0545

    added serialzers and other section

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Category, Product
+from .models import Cart, Category, Coupon, Product
 
 
 @admin.register(Category)
@@ -12,3 +12,15 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price_in_npr', 'available_product', 'offer_price', 'is_offer_valid')
     list_filter = ('category', 'available_offer_start_date', 'available_offer_end_date')
     search_fields = ('name', 'category__name')
+
+@admin.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('code', 'discount_percent', 'valid_from', 'valid_to')
+    list_filter = ('valid_from', 'valid_to')
+    search_fields = ('code',)
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'category', 'product', 'price', 'battery_health_condition', 'coupon_code')
+    list_filter = ('category', 'coupon_code')
+    search_fields = ('user__username', 'product__name', 'coupon_code__code')
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -9,7 +9,6 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-
 class Product(models.Model):
     photo = models.ImageField(upload_to='product_photos/', null=True, blank=True)
     name = models.CharField(max_length=200)
@@ -40,3 +39,39 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20, unique=True)
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
+    valid_from = models.DateField()
+    valid_to = models.DateField()
+
+    def is_valid(self):
+        now = timezone.now().date()
+        return self.valid_from <= now <= self.valid_to
+
+    def __str__(self):
+        return self.code
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    battery_health_condition = models.CharField(max_length=100)
+    coupon_code = models.ForeignKey(Coupon, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def apply_discount(self):
+        if self.coupon_code and self.coupon_code.is_valid():
+            discount_amount = (self.price * self.coupon_code.discount_percent) / 100
+            discounted_price = self.price - discount_amount
+            return discounted_price
+        return self.price
+
+    def save(self, *args, **kwargs):
+        if self.coupon_code and self.coupon_code.is_valid():
+            self.price = self.apply_discount()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.product.name} in {self.category.name}"
