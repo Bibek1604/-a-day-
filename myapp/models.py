08d3488355c5e380b35ce76ceac721da566e2225
Bibek1604 <npbibek94@gmail.com>
2024-06-05 09:55:58 -0900

    folderstructure changed

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 # myapp/models.py
 from django.db import models
 from django.utils import timezone
+from django.core.validators import FileExtensionValidator
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -8,10 +9,11 @@ class Product(models.Model):
     initial_rate = models.DecimalField(max_digits=10, decimal_places=2)
     final_rate = models.DecimalField(max_digits=10, decimal_places=2)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2)
-    photo = models.ImageField(upload_to='productsImage only')
+    feature_image = models.ImageField(upload_to='feature_product_images/', default='path/to/default/image.jpg')
     color = models.CharField(max_length=50)
     available = models.BooleanField(default=True)
     stock = models.IntegerField()
+    Category = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -23,8 +25,10 @@ class FeatureProduct(models.Model):
     initial_price = models.DecimalField(max_digits=10, decimal_places=2)
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2, editable=False)
-    feature_image = models.ImageField(upload_to='feature_product_images/', null=True, blank=True)
+    feature_image = models.ImageField(upload_to='feature_product_images/', default='feature_product_images/default_image.jpg')
     sale_end_time = models.DateTimeField()
+    Category = models.CharField(max_length=500, null=True, blank=True)
+
 
     def save(self, *args, **kwargs):
         # Calculate discount percent
