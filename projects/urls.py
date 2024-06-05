4834d5c2b4f44eafcfcfd8cf7efe464903ba181b
Bibek1604 <npbibek94@gmail.com>
2024-06-05 09:41:06 -0900

    setting and urls file updated

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,10 @@
 # projects/urls.py
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),  # URL for the admin interface
     path('products/', include('myapp.urls')),  # Include URLs from the myapp
-]
+    path('products/', include('myapp.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
