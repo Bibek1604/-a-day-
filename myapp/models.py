90554259aa641af7aa9e1d1428a6707df72c5a02
Bibek1604 <npbibek94@gmail.com>
2024-06-05 09:43:24 -0900

    featured image file added

--- myapp/models.py
+++ myapp/models.py
@@ -17,22 +17,20 @@ class Product(models.Model):
         return self.title
 # myapp/models.py
 from django.db import models
-from django.utils import timezone
-
 class FeatureProduct(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     initial_price = models.DecimalField(max_digits=10, decimal_places=2)
     final_price = models.DecimalField(max_digits=10, decimal_places=2)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2, editable=False)
-    feature_image = models.ImageField(upload_to='feature_product_images/')
+    feature_image = models.ImageField(upload_to='feature_product_images/', null=True, blank=True)
     sale_end_time = models.DateTimeField()
 
     def save(self, *args, **kwargs):
         # Calculate discount percent
         if self.initial_price > 0:
             self.discount_percent = ((self.initial_price - self.final_price) / self.initial_price) * 100
-        super().save(*args, **kwargs)
+        super(FeatureProduct, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
