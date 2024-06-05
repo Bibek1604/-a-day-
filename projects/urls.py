c9b5defd91cfdd2f0dbd6b31b548c2fe357ea2c0
Bibek1604 <npbibek94@gmail.com>
2024-06-05 12:50:51 -0900

    migration done up to flashsales for reminder

--- projects/urls.py
+++ projects/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from myapp.views import FeatureProductListView, feature_product_detail_view
 urlpatterns = [
     path('admin/', admin.site.urls),  # URL for the admin interface
     path('products/', include('myapp.urls')),  # Include URLs from the myapp
     path('Featureproducts/', include('myapp.urls')),
+    path('BestSellingProducts/', include('myapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
