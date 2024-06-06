179fb28e6620baafcd3639037de6ee8b66097be4
Bibek1604 <npbibek94@gmail.com>
2024-06-05 21:48:47 -0900

    urls file updated

--- projects/urls.py
+++ projects/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('products/', include('myapp.urls')),  # Include URLs from the myapp
     path('Featureproducts/', include('myapp.urls')),
     path('BestSellingProducts/', include('myapp.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
