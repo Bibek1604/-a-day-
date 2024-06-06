0eeab2f55d125c802846d88b1dee93af868b9a5d
Bibek1604 <npbibek94@gmail.com>
2024-06-05 22:11:09 -0900

    setting & cors error handeling

--- projects/urls.py
+++ projects/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('products/', include('myapp.urls')),  # Include URLs from the myapp
     path('Featureproducts/', include('myapp.urls')),
     path('BestSellingProducts/', include('myapp.urls')),
+    path('coupencode/', include('myapp.urls')), 
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
