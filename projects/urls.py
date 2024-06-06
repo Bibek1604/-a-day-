068d42ca1c7575a7652aed6136c21d423357f7d5
Bibek1604 <npbibek94@gmail.com>
2024-06-05 23:13:23 -0900

    updated the url file bascically routing

--- projects/urls.py
+++ projects/urls.py
@@ -4,11 +4,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from myapp.views import FeatureProductListView, feature_product_detail_view
+from myapp.views import create_order
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),  # URL for the admin interface
     path('products/', include('myapp.urls')),  # Include URLs from the myapp
     path('Featureproducts/', include('myapp.urls')),
     path('BestSellingProducts/', include('myapp.urls')),
     path('coupencode/', include('myapp.urls')), 
+    path('api/create-order/', create_order, name='create_order'),  # Named URL pattern
+
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
