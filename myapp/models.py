1c96677c910c306aa271825dd152d127c85fae91
Bibek1604 <npbibek94@gmail.com>
2024-06-09 15:39:14 +0545

    panels added

--- myapp/models.py
+++ myapp/models.py
@@ -49,16 +49,13 @@ class Product(models.Model):
     initial_rate = models.DecimalField(max_digits=10, decimal_places=2)
     final_rate = models.DecimalField(max_digits=10, decimal_places=2)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    image = models.ImageField(upload_to='product_images/', default='path/to/default/image.jpg')
-    thumbnail1 = models.ImageField(upload_to='product_images/', default='path/to/default/image.jpg')
-    thumbnail2 = models.ImageField(upload_to='product_images/', default='path/to/default/image.jpg')
-    thumbnail3 = models.ImageField(upload_to='product_images/', default='path/to/default/image.jpg')
-    
+    photo = models.ImageField(upload_to='product_images/', default='path/to/default/image.jpg')
+
     color = models.CharField(max_length=50, choices=COLOR_CHOICES, default='Red')
     available = models.BooleanField(default=True)
     stock = models.IntegerField(null=True)
     category = models.CharField(max_length=500, choices=CATEGORY_CHOICES, null=True, blank=True)
-    warrenty = models.CharField(max_length=100, null=True, blank=True)
+    warranty = models.CharField(max_length=100, null=True, blank=True)
     storage = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
@@ -67,23 +64,23 @@ class Product(models.Model):
 class FeatureProduct(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    initial_price = models.DecimalField(max_digits=10, decimal_places=2)
-    final_price = models.DecimalField(max_digits=10, decimal_places=2)
+    initial_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    final_rate = models.DecimalField(max_digits=10, decimal_places=2)
     discount_percent = models.DecimalField(max_digits=5, decimal_places=2, editable=False, default=0)
     discount_amount = models.DecimalField(max_digits=10, decimal_places=2, editable=False, default=0)
-    feature_image = models.ImageField(upload_to='feature_product_images/', default='feature_product_images/default_image.jpg')
+    photo = models.ImageField(upload_to='feature_product_images/', default='feature_product_images/default_image.jpg')
     sale_end_time = models.DateTimeField()
     category = models.CharField(max_length=500, choices=CATEGORY_CHOICES, null=True, blank=True)
     available = models.BooleanField(default=True)
     stock = models.IntegerField(null=True)
     color = models.CharField(max_length=50, choices=COLOR_CHOICES, default='Red')
-    warrenty = models.CharField(max_length=100, null=True, blank=True)
+    warranty = models.CharField(max_length=100, null=True, blank=True)
     storage = models.CharField(max_length=100, null=False, blank=True)
 
     def save(self, *args, **kwargs):
-        if self.initial_price > 0:
-            self.discount_percent = ((self.initial_price - self.final_price) / self.initial_price) * 100
-            self.discount_amount = self.initial_price - self.final_price
+        if self.initial_rate > 0:
+            self.discount_percent = ((self.initial_rate - self.final_rate) / self.initial_rate) * 100
+            self.discount_amount = self.initial_rate - self.final_rate
         super(FeatureProduct, self).save(*args, **kwargs)
 
     def __str__(self):
@@ -94,12 +91,12 @@ class BestSellingProduct(models.Model):
     description = models.TextField()
     initial_rate = models.DecimalField(max_digits=10, decimal_places=2)
     final_rate = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='best_selling_product_images/', default='best_selling_product_images/default_image.jpg')
+    photo = models.ImageField(upload_to='best_selling_product_images/', default='best_selling_product_images/default_image.jpg')
     color = models.CharField(max_length=50, choices=COLOR_CHOICES, default='Red')
     available = models.BooleanField(default=True)
     stock = models.IntegerField(null=True)
     category = models.CharField(max_length=500, choices=CATEGORY_CHOICES, null=True, blank=True)
-    warrenty = models.CharField(max_length=100, null=True, blank=True)
+    warranty = models.CharField(max_length=100, null=True, blank=True)
     storage = models.CharField(max_length=100, null=False, blank=True)
 
     def __str__(self):
@@ -108,19 +105,25 @@ class BestSellingProduct(models.Model):
 class FlashSale(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    initial_price = models.DecimalField(max_digits=10, decimal_places=2)
-    final_price = models.DecimalField(max_digits=10, decimal_places=2)
+    initial_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    final_rate = models.DecimalField(max_digits=10, decimal_places=2)
     photo = models.ImageField(upload_to='flash_sale_photos/', default='flash_sale_photos/default_photo.jpg')
     remaining_time = models.DateTimeField()
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
     product_id = models.CharField(max_length=50)
-    warrenty = models.CharField(max_length=100, null=True, blank=True)
+    warranty = models.CharField(max_length=100, null=True, blank=True)
     storage = models.CharField(max_length=100, null=False, blank=True)
 
     def time_remaining(self):
         now = timezone.now()
         remaining_time = self.remaining_time - now
-        return remaining_time.total_seconds() // 3600, (remaining_time.total_seconds() // 60) % 60
+        if remaining_time.total_seconds() < 0:
+            return 0, 0, 0, 0  # Sale has ended
+
+        days = remaining_time.days
+        hours, remainder = divmod(remaining_time.seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return days, hours, minutes, seconds
 
     def __str__(self):
         return self.title
